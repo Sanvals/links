5e142da7d8ed91495803b5f0bb495ca637744177
Sanvals <santano.sergio@gmail.com>
2024-10-28 13:03:42 +0100

    json file to main, added skipRender tag

--- scripts/jsonloader.js
+++ scripts/jsonloader.js
@@ -1,19 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Fetch links from JSON file
-    fetch('scripts/links.json')
+    fetch('./links.json')
         .then(response => response.json())
         .then(data => {
             const container = document.querySelector('main');
 
             // Loop through each category in the JSON data
             Object.keys(data).forEach(category => {
+                const categoryData = data[category];
+                
+                // Check if category has skipRender set to true, if so, skip rendering
+                if (categoryData.skipRender) return;
+
                 const detailsElement = document.createElement('details');
                 const summaryElement = document.createElement('summary');
                 summaryElement.textContent = category;
 
                 const sectionElement = document.createElement('section');
 
-                data[category].forEach(linkData => {
+                categoryData.links.forEach(linkData => {
                     const linkElement = document.createElement('a');
                     linkElement.href = linkData.url;
                     linkElement.target = "_blank";
