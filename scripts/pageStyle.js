8661b9113fc9dfa2da9519d669377746add03055
Sanvals <santano.sergio@gmail.com>
2024-11-01 17:45:07 +0100

    added green style

--- scripts/pageStyle.js
+++ scripts/pageStyle.js
@@ -7,15 +7,24 @@ let currentMood = 1;
 const moods = {
     mood1: {
         footerImg: 'https://my.logiscool.com/static/media/space-footer.1b18286a.png',
+        mainColor: '#cecece',
         buttonColor: 'rgb(36, 47, 112)',
         buttonShadow: 'rgb(21, 32, 83)',
         backColor: 'linear-gradient(rgb(12, 14, 39), rgba(18, 31, 66, 0.83)) 0% 0% / auto fixed, url(https://my.logiscool.com/static/media/space-bg.af3c9b72.jpg) 0% 0% / 1270px'
     },
     mood2: {
         footerImg: 'https://my.logiscool.com/static/media/halloween_footer.09088135.webp',
+        mainColor: '#cecece',
         buttonColor: 'rgb(181, 71, 5)',
         buttonShadow: 'rgb(153, 35, 35)',
         backColor: 'fixed rgb(67, 35, 95)'
+    },
+    mood3: {
+        footerImg: 'https://my.logiscool.com/static/media/easter-footer.e30df72f.webp',
+        mainColor: 'rgb(0, 0, 0)',
+        buttonColor: 'rgba(0, 0, 0, 0.04)',
+        buttonShadow: 'rgba(0, 0, 0, 0.2)',
+        backColor: 'linear-gradient(rgb(107, 217, 225), rgb(233, 251, 252)) fixed'
     }
 };
 
@@ -24,6 +33,7 @@ function changeMood(mood) {
     const target = moods[mood]
 
     footerImg.src = target.footerImg;
+    document.documentElement.style.setProperty('--main-color', target.mainColor);
     document.documentElement.style.setProperty('--button-color', target.buttonColor);
     document.documentElement.style.setProperty('--background-color', target.backColor);
     document.documentElement.style.setProperty('--button-shadow', target.buttonShadow);
